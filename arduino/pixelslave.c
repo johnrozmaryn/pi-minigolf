01fc167516811e55e39cb814ffc1cfff621b8270
John Rozmaryn <jrozmaryn@beckman.com>
2019-05-28 13:53:07 -0400

    Binary explorations from lunch

---
+++ arduino/pixelslave.c
@@ -0,0 +1,40 @@
+cSpace = (0,0,255)
+cReality = (255,0,0)
+cPower = (150,0,150)
+cMind = (255,255,0)
+cTime = (0,255,0)
+cSoul = (255,80,0)
+
+int pSpace = 0
+int rSpace = 0
+int gSpace = 0
+int bSpace = 255
+
+int pReality = 1
+int rReality = 255
+int gReality = 0
+int bReality = 0
+
+int pPower = 2
+int rPower = 150
+int gPower = 0
+int bPower = 150
+
+int pMind = 3
+int rMind = 255
+int gMind = 255
+int bMind = 0
+
+int pTime = 4
+int rTime = 0
+int gTime = 255
+int bTime = 0
+
+int pSoul = 5
+int rSoul = 255
+int gSoul = 80
+int bSoul = 0
+
+pixels.setPixelColor(i, pixels.Color(0,150,0));
+
+
